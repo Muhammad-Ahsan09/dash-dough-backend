1509817dafa1c4b24d2819c893a6e78b6564e044
Venom <mmahsan322@gmail.com>
2026-02-21 23:30:27 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import authRouter from "./routes/auth-routes.js";
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+import { MongoClient, ServerApiVersion } from "mongodb";
+import { createPayment } from "./controllers/payment-controller.js";
+import paymentRouter from "./routes/payment-routes.js"
+
+
+dotenv.config()
+
+const connectDB = async () => {
+    try {
+      await mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+  
+      console.log("MongoDB connected successfully");
+    } catch (error) {
+      console.error("MongoDB connection error:", error.message);
+      process.exit(1); // stop server if DB fails
+    }
+};
+
+await connectDB()
+
+
+const app = express();
+
+app.use(express.json()); // for application/json
+app.use(express.urlencoded({ extended: true })); // for form-data or URL-encoded
+
+app.use('/api/auth', authRouter);
+app.use('/api/payment', paymentRouter)
+
+
+
+app.listen(8000,   () => {
+    console.log("Server has started")
+})
